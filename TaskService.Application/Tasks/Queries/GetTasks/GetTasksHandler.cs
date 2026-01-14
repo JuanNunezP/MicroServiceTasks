15cd6241f4917b9e467370bbe8f916f38be071a4
Juan N <jnunezpinilla@gmail.com>
2026-01-13 20:57:27 -0500

    test error

--- TaskService.Application/Tasks/Queries/GetTasks/GetTasksHandler.cs
+++ TaskService.Application/Tasks/Queries/GetTasks/GetTasksHandler.cs
@@ -14,6 +14,6 @@ namespace TaskService.Application.Tasks.Queries.GetTasks
         public GetTasksHandler(ITaskRepository repo) => _repo = repo;
 
         public async Task<List<TaskItem>> HandleAsync(GetTasksQuery _)
-            => await _repo.GetAllAsync();
+            => await _repo.GetAllAsyn();
     }
 }
